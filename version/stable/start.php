aa077d65f5cd055cbbccb25519342f433d7335d9
roman <roman@zhilk.in>
2021-10-12 00:09:50 +0700

    0.0.2 ALPHA fix

--- version/stable/start.php
+++ version/stable/start.php
@@ -16,16 +16,16 @@ $GLOBALS['HOME'] = [
 
 
 
-$HOME = [
-    'dir' => __DIR__,
-    'style' => 'version/'.basename(__DIR__).'/system/pages/assets/css/style.css',
-    'version' => '<strong></strong> 0.0.1 STABLE | <strong>date:</strong> 08.10.2021',
-    'changeLog' => '
-    ..:: ЖУРНАЛ ::.
-    1. Первая стабильная версия сайта.
-    ',
-
-];
+//$HOME = [
+//    'dir' => __DIR__,
+//    'style' => 'version/'.basename(__DIR__).'/system/pages/assets/css/style.css',
+//    'version' => '<strong></strong> 0.0.1 STABLE | <strong>date:</strong> 08.10.2021',
+//    'changeLog' => '
+//    ..:: ЖУРНАЛ ::.
+//    1. Первая стабильная версия сайта.
+//    ',
+//
+//];
 
 
 
@@ -35,17 +35,17 @@ $HOME = [
 
 
 // Подключаем библиотеку 'my'.
-if (!include $HOME['dir'].'/system/lib/autoload.php') {
+if (!include $GLOBALS['HOME']['dir'].'/system/lib/autoload.php') {
   // Если не удалось подключить библиотеки, выводим сообщение.
-  echo "ОШИБКА: Не удалось подключить библиотеки в версии: ".$HOME['version'].".";
+  echo "ОШИБКА: Не удалось подключить библиотеки в версии: ".$GLOBALS['HOME']['version'].".";
   // Завершаем скрипт.
   exit;
 }
 
 // Подключаем ядро сайта.
-if (!include $HOME['dir'].'/system/logic/core.php') {
+if (!include $GLOBALS['HOME']['dir'].'/system/logic/core.php') {
   // Если не удалось подключить ядро сайта, выводим сообщение.
-  echo "ОШИБКА: Не удалось подключить ядро сайта в версии: ".$site_info_version[0].".";
+  echo "ОШИБКА: Не удалось подключить ядро сайта в версии: ".$GLOBALS['HOME']['version'].".";
   // Завершаем скрипт.
   exit;
 }
